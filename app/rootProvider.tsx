6edaf10b16767dd7ea83d4aca5a85cf1aa339fb6
Hopscup <139771459+hopscup@users.noreply.github.com>
2026-01-18 01:33:43 +0300

    Update rootProvider.tsx

--- app/rootProvider.tsx
+++ app/rootProvider.tsx
@@ -1,12 +1,22 @@
 "use client";
 import { ReactNode } from "react";
-import { MiniKitProvider } from "@coinbase/onchainkit/minikit";
+import { base } from "wagmi/chains";
+import { OnchainKitProvider } from "@coinbase/onchainkit";
 import "@coinbase/onchainkit/styles.css";
 
 export function RootProvider({ children }: { children: ReactNode }) {
   return (
-    <MiniKitProvider>
+    <OnchainKitProvider
+      apiKey={process.env.NEXT_PUBLIC_ONCHAINKIT_API_KEY}
+      chain={base}
+      config={{
+        appearance: {
+          mode: "auto",
+        },
+      }}
+      projectId={process.env.NEXT_PUBLIC_CDP_PROJECT_ID}
+    >
       {children}
-    </MiniKitProvider>
+    </OnchainKitProvider>
   );
 }
